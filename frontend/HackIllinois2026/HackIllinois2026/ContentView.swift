14bd3db02dba0bc92a48a996684038b60ac36dbd
Om Chachad <omchachad12@gmail.com>
2026-02-27 22:10:23 -0600

    Added Initial Xcode Project

---
+++ frontend/HackIllinois2026/HackIllinois2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackIllinois2026
+//
+//  Created by Om Chachad on 27/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ frontend/HackIllinois2026/HackIllinois2026/HackIllinois2026App.swift
@@ -0,0 +1,17 @@
+//
+//  HackIllinois2026App.swift
+//  HackIllinois2026
+//
+//  Created by Om Chachad on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct HackIllinois2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
